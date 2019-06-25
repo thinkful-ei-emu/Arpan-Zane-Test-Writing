eddaaa888560686dadf4e4bcde9cf5be1e2432a3
Arpan Patel <adp8ke@gmail.com>
2019-06-25 14:10:07 -0400

    fixed list.test.js

--- trelloyes/Adrienne-Zane-Trelloyes/src/List.test.js
+++ trelloyes/Adrienne-Zane-Trelloyes/src/List.test.js
@@ -2,20 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import List from './List';
-import Card from './Card';
-import App from './App';
+import STORE from './store';
+
+const list ={
+    id: '1',
+    header: 'First list',
+    cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
+  };
 
 describe('<List/>',()=>{
     it('renders without crashing',()=>{
         const div= document.createElement('div');
 
-        ReactDOM.render(<List /> ,div);
+        ReactDOM.render((<List key= {list.id} title={list.header} cards={list.cardIds.map(id => STORE.allCards[id])} />), div);
 
         ReactDOM.unmountComponentAtNode(div);
     });
 
     it('renders this UI as Expected',()=>{
-        const tree=renderer.create(<List/>).toJSON();
+        const tree = renderer.create((<List key= {list.id} title={list.header} cards={list.cardIds.map(id => STORE.allCards[id])} />)).toJSON();
         expect(tree).toMatchSnapshot();
     });
 });
